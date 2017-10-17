95a84db24792a18c6e07d2f6f8ddec555f6e1d17
Kornetzky <5hf-haz-u1@pool314pc15.edu.inform.hs-hannover.de>
2017-10-17 08:45:13 +0200

    test

---
+++ HDRS_GF/T/Floor.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Floor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Floor extends Actor
+{
+    /**
+     * Act - do whatever the Floor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    { new GreenfootImage("Dirt.png");
+        
+       
+        
+        // Add your action code here.
+    }    
+}
---
+++ HDRS_GF/T/MyWorld.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    
+    private Floor[][] array = new Floor[1000][1000];
+    
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixel
+        
+        super(1024,640, 1); 
+        
+       
+        for(int y = 0;y<=640;y += 32){
+            for (int x = 0; x<=640; x += 32){
+                Floor temp = new Floor();
+                array[x][y] = temp;
+                addObject(temp ,x,y);
+                System.out.println(x +" " + y);
+            }
+        }
+    }
+    
+    public Floor[][] getAllFloors(){
+        return array;
+    }
+}
